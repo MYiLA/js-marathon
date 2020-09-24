1378bd70803d664a6eea1ce3842cd613135dd0e6
Дюбанова Мария Борисовна <dyubanova.mb@esphere.local>
2020-09-24 19:17:51 +0300

    clear

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
-const firstRow = 'мама мыла раму';
-const SecondRow = 'собака друг человека';
 
-const calcSymbol = (str, symbol) => {
-  let result = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) === symbol) {
-        result += 1
-    }
-  }
-  return result
-}
-
-const getRow = (str1, str2) => {
-  return (calcSymbol(str1, 'a') > calcSymbol(str2, 'a')) ? str1 : str2
-}
-
-console.log(getRow(firstRow, SecondRow))
